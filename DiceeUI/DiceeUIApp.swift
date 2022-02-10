c1d0d7571cde8d871bf3482415933a186248af1b
Music Player <rlacnrrnrhd@naver.com>
2022-02-10 15:37:18 +0900

    Initial Commit

---
+++ DiceeUI/DiceeUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  DiceeUIApp.swift
+//  DiceeUI
+//
+//  Created by 성제 on 2022/02/10.
+//
+
+import SwiftUI
+
+@main
+struct DiceeUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
